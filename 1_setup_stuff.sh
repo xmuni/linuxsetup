c7e00fd7b34950ae08c37da38b655e81a49d787c
xmuni <vitto.m93@gmail.com>
2018-10-10 19:06:40 +0200

    Tweaks

--- 1_setup_stuff.sh
+++ 1_setup_stuff.sh
@@ -13,3 +13,12 @@ echo "- Fonts installed"
 # sed -i 's/before/after/g' file.txt
 sudo sed -i 's/env_reset/env_reset,pwfeedback/g' /etc/sudoers
 echo "- Added asterisks for terminal password input"
+
+# BASH ALIASES AND CTRL+BACKSPACE BIND
+
+sudo echo "alias neo='neofetch'" >> ~/.bashrc
+sudo echo "alias upd='sudo apt update'" >> ~/.bashrc
+sudo echo "alias upg='sudo apt upgrade'" >> ~/.bashrc
+sudo echo "alias updg='sudo apt update && sudo apt upgrade'" >> ~/.bashrc
+sudo echo "bind '"0x08":backward-kill-word' >> ~/.bashrc
+echo "- Added aliases and ctrl+backspace bind"
